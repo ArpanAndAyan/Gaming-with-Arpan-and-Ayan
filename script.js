8d5791d24a1f76e17ce0bda578c3c5ae61576808
Pallab Chakraborty <gamingwitharpanandayan@gmail.com>
2021-10-02 12:40:07 +0530

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-      (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/60ab1232bbd5354c0fdbb390/1f6e3u63h';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-      })();
+<script type="text/javascript"> window.$crisp=[];window.CRISP_WEBSITE_ID="9b027315-daf8-4aa1-866c-e93a65ff8e86";(function(){ d=document;s=d.createElement("script"); s.src="https://client.crisp.chat/l.js"; s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})(); </script>
