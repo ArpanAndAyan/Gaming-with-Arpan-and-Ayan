067d5a99abaa5ece7570d76ce86b97cf9316d375
ArpanAndAyan <91407985+ArpanAndAyan@users.noreply.github.com>
2021-11-05 19:01:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 window.$crisp=[];window.CRISP_WEBSITE_ID="9b027315-daf8-4aa1-866c-e93a65ff8e86";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+
+function myFunction(x) {
+    x.classList.toggle("change");
+
+
+    // if(document.getElementById("toggleitem").style.display == "none") {
+    //     document.getElementById("toggleitem").style.display = "block";
+    // } else {
+    //     document.getElementById("toggleitem").style.display = "none";
+    // }
+
+    // if(document.getElementById("toggleitem2").style.display == "none") {
+    //     document.getElementById("toggleitem2").style.display = "block";
+    // } else {
+    //     document.getElementById("toggleitem2").style.display = "none";
+    // }
+}
